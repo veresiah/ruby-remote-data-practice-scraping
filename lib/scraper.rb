5f7e84e5330b8b6224966399059440c58291177c
Veresia Hiralall <hiralallveresia@yahoo.com>
2020-11-30 11:03:55 -0500

    commit tree 56a3b6b75a6c0de59521b4ab402d837ed97a18f0

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+
+puts doc.css(".headline-260IBN").text
